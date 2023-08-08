f998465e76871790bc2afa7e73e9c4f5e2131ef4
zaragood <azaragoodness52@gmail.com>
2023-08-08 06:16:24 -0700

    without using f string

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 # iprogram that prints the ASCII alphabet, in lowercase
-alphabets = ""
 for letter in range(97, 123):
-    alphabets += chr(letter)
-print("{}".format(alphabets))
+    print("{}".format(chr(letter)), end="")
