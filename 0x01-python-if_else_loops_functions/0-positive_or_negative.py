e5d631a0e70c45d581d54c7ffe587677074eff0b
zaragood <azaragoodness52@gmail.com>
2023-08-08 04:51:06 -0700

    spelling correction

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import random
-# print whether the number stored in the variable number
+# print whether the number stored in the variable numbei
 # is positive or negative
 number = random.randint(-10, 10)
 
 if number < 0:
-    print(f"{number} is negetive")
+    print(f"{number} is negative")
 elif number > 0:
     print(f"{number} is positive")
 else:
--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,11 @@
-
+#!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
 
 lastDigit = abs(number) % 10
 if lastDigit > 5:
     print(f"Last digit of {number} is {lastDigit} and is greater than 5")
-elif last_digit < 6 and last_digit != 0:
+elif lastDigit < 6 and lastDigit != 0:
     print(f"Last digit of {number} is {lastDigit} and is less than 6 and not 0")
 else:
     print(f"Last digit of {number} is {lastDigit} and is 0")
