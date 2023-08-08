50b4dfda0d0a10654cc92d1688278d1d1263bed7
zaragood <azaragoodness52@gmail.com>
2023-08-08 06:20:07 -0700

    changes f string

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,9 +2,7 @@
 # program that prints the ASCII alphabet, in lowercase
 # Print all the letters except q and e
 
-alphabets = ""
 for letter in range(97, 123):
     alpha = chr(letter)
     if alpha != 'q' and alpha != 'e':
-        alphabets += alpha
-print(alphabets)
+        print("{}".format(alpha), end="")
