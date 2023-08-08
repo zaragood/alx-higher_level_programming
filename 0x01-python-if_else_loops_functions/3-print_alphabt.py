e01048a2394efca67f5169cd5a2cb56348f0837f
zaragood <azaragoodness52@gmail.com>
2023-08-08 03:34:47 -0700

    program that Print all the letters except q and e

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,9 @@
+#program that prints the ASCII alphabet, in lowercase
+#Print all the letters except q and e
+
+alphabets = ""
+for letter in range(97, 123):
+    alpha = chr(letter)
+    if alpha != 'q' and alpha != 'e':
+        alphabets += alpha
+print(alphabets)
