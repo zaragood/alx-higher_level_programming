2817923194003ed893aae089763b8fb3b344d19e
zaragood <azaragoodness52@gmail.com>
2023-08-12 09:27:13 -0700

    updated

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """program that imports the function def add(a, b):
-    from the file add_0.py and prints the result
-    of the addition 1 + 2 = 3
-    """
     from add_0 import add
     a = 1
     b = 2
