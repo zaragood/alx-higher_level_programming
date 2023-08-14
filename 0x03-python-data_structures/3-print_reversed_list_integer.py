a906394d539b2a4f0060c4c87a123f85e4963a78
zaragood <azaragoodness52@gmail.com>
2023-08-14 06:45:38 -0700

    checking if my list is not None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,7 @@
 # function that prints all integers of a list, in reverse order
 
 def print_reversed_list_integer(my_list=[]):
-    reversed_list = my_list[::-1]
-    for item in reversed_list:
-        print("{}".format(item))
+    if my_list is not None:
+        reversed_list = my_list[::-1]
+        for item in reversed_list:
+            print("{:d}".format(item))
