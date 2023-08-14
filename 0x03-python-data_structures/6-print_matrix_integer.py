ba46e8c4750d1105979a10b272b6b4622c1e3d05
zaragood <azaragoodness52@gmail.com>
2023-08-14 08:29:29 -0700

    if check

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,8 @@
 # function that prints a matrix of integers.
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for item in row:
-            print("{:d}".format(item), end=" ")
-        print()
+    if matrix is not None:
+        for row in matrix:
+            for item in row:
+                print("{:d}".format(item), end=" ")
+            print()
