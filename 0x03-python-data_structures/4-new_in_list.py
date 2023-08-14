968471e20934ee81ef5c8e6b411f9c60fb171e37
zaragood <azaragoodness52@gmail.com>
2023-08-14 07:20:12 -0700

    added if checks

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,7 +7,10 @@
 
 def new_in_list(my_list, idx, element):
     new_list = []
-    for item in my_list:
-        new_list.append(item)
-    new_list[idx] = element
-    return (new_list)
+    if (idx < 0) or (idx >= len(my_list)):
+        return (my_list)
+    else:
+        for item in my_list:
+            new_list.append(item)
+        new_list[idx] = element
+        return (new_list)
