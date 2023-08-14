e62dd6f140c5da18486eb4f01ff89e4f3c507983
zaragood <azaragoodness52@gmail.com>
2023-08-14 05:56:35 -0700

    reduce code lines

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,9 +3,7 @@
 # Function that retrieves an element from a list
 def element_at(my_list, idx):
     list_len = len(my_list)
-    if idx < 0:
-        return (None)
-    elif idx > list_len - 1:
+    if (idx < 0) or (idx > list_len - 1):
         return (None)
     else:
         return(my_list[idx])
