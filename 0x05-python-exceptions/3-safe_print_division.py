76f852c40648d465e656f3aaf2705e591102950f
zaragood <azaragoodness52@gmail.com>
2023-08-29 07:17:09 -0700

    zero division error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    result = 0
-
     try:
         result = a / b
-        return (result)
-    except (TypeError, ValueError, ZeroDivisionError):
-        return (None)
+    except ZeroDivisionError:
+        result = None
     finally:
-        print("Inside result: {:.1f}".format(result))
+        print("Inside result: {}".format(result))
+        return (result)
 
