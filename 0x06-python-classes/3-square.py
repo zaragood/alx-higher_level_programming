3f67740f3340e6c45e26bb1035605deea32fac67
zaragood <azaragoodness52@gmail.com>
2023-08-30 08:03:49 -0700

    class method that cal the area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""defines a class Square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initializing a class attributes
+
+        Args:
+            size (int): size of the square.
+        """
+        if isinstance(size, int):
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+
+    def area(self):
+        """Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return (self.__size * self.__size)
