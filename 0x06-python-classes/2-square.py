2ca53c2810657ecf7e0a9601f5ab316f105d68ed
zaragood <azaragoodness52@gmail.com>
2023-08-30 07:32:47 -0700

    a class Square that defines a square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """represents a class Square"""
+
+    def __init__(self, size=0):
+        """initializing a private attribute 'size'
+
+        Args:
+            size (int): size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
