efe5bd5e1bcafe4ab199224406b902186970fc88
zaragood <azaragoodness52@gmail.com>
2023-08-30 07:38:08 -0700

    check value error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,7 +14,8 @@ class Square:
         if type(size) is not int:
             raise TypeError("size must be integer")
 
+        else:
+            self.__size = size
+
         if size < 0:
             raise ValueError("size must be >= 0")
-
-        self.__size = size
