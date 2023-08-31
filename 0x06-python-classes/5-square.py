443c050594436f1245fc46871fb47e136f5729c5
zaragood <azaragoodness52@gmail.com>
2023-08-31 06:42:32 -0700

    added whitespace

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -16,7 +16,7 @@ class Square:
     @property
     def size(self):
         """getting the privat instance attribute size"""
-        return(self.__size)
+        return (self.__size)
 
     @size.setter
     def size(self, value):
