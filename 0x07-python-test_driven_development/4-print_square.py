e9d0dacd3d4c3b1648461b726ab3f5cd92611711
zaragood <azaragoodness52@gmail.com>
2023-09-04 13:23:17 +0000

    doctring for print_square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a function print_square"""
+
+
+def print_square(size):
+    """
+    function that prints a square with the character #
+
+        Args:
+            @size (int): size of the square
+    """
+    if not isinstance(size, int) or isinstance(size, float):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for _ in range(size):
+        print("#" * size)
+    print()
