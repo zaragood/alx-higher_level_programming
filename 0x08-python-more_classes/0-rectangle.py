58c11ad313bc6b22cda5afcd40f3a3d76b39e8c8
zaragood <azaragoodness52@gmail.com>
2023-09-05 15:55:17 +0000

    empty class Rectangle that defines a rectangle:

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines an empty class"""
+
+
+class Rectangle:
+    """represents a class object 'Rectangle"""
+
+    def __init__(self):
+        """An empty class instance"""
+        pass
