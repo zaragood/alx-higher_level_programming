811ffec1fdf2c1f1ee3bf2c5a862100e2810bafd
zaragood <azaragoodness52@gmail.com>
2023-09-05 21:50:55 +0000

    removed multiple spacing

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -51,4 +51,4 @@ class Rectangle:
         """return the perimeter of a rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__width +  self.__height)
+        return 2 * (self.__width + self.__height)
