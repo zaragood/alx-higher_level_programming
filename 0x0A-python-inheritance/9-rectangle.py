2a9f816e6874dc6f4746cc1f0a74beee86b46196
zaragood <azaragoodness52@gmail.com>
2023-09-13 15:04:22 +0000

    inheritence git push

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""creating a class Rectangle that inherits from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """definning a class Rectangle"""
+
+    def __init__(self, width, height):
+        """Instantiation with width and height"""
+
+        self.integer_validator('width', width)
+        self.integer_validator('heigth', height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        a = str(self.__width)
+        b = str(self.__height)
+        return "[Rectangle]" + " " + a + "/" + b
