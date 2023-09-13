d65727eb4c67f471b5366d9ce75040c504f5b834
zaragood <azaragoodness52@gmail.com>
2023-09-13 12:57:13 +0000

    class BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""creating an empty class"""
+
+
+class BaseGeometry:
+    """An empty class BaseGeometry"""
+
+    def area(self):
+        """Public instance method: def area(self):
+        that raises an Exception"""
+        raise Exception("area() is not implemented")
