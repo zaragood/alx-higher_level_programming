5c7cdb0ccfe292e05862b366c1907db11ec90fe5
zaragood <azaragoodness52@gmail.com>
2023-09-13 11:55:04 +0000

    pycodestyle

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""creating a class MyList"""
+"""module to creating a class MyList"""
+
 
 class MyList(list):
+    """creating a class MyList"""
     def print_sorted(self):
         print(sorted(self))
         return sorted(self)
