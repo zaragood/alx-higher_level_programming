ca62c55e629f3ac63d1332f576e1b47ae1585657
zaragood <azaragoodness52@gmail.com>
2023-09-16 22:41:31 +0000

    pep8 valisation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -64,7 +64,7 @@ class Rectangle(Base):
         if not isinstance(x, int):
             raise TypeError("x must be an integer")
         if x < 0:
-             raise ValueError("x must be >= 0")
+            raise ValueError("x must be >= 0")
         self.__x = x
 
     @property
@@ -78,5 +78,5 @@ class Rectangle(Base):
         if not isinstance(y, int):
             raise TypeError("y must be an integer")
         if y < 0:
-             raise ValueError("y must be >= 0")
+            raise ValueError("y must be >= 0")
         self.__y = y
