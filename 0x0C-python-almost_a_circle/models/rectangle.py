0f3a1947a71ccc0a31347059113a814db0c53923
zaragood <azaragoodness52@gmail.com>
2023-09-16 22:29:50 +0000

    Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -33,6 +33,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         """sets the attribute width"""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
         self.__width = width
 
     @property
@@ -43,6 +47,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, height):
         """sets the attribute of height"""
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
 
     @property
@@ -50,9 +58,13 @@ class Rectangle(Base):
         """getting the private instance attribute x"""
         return (self.__x)
 
-    @height.setter
+    @x.setter
     def x(self, x):
         """sets the attribute of x"""
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if x < 0:
+             raise ValueError("x must be >= 0")
         self.__x = x
 
     @property
@@ -63,4 +75,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, y):
         """sets the attribute of y"""
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        if y < 0:
+             raise ValueError("y must be >= 0")
         self.__y = y
