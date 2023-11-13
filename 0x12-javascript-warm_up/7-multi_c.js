9d0c40a78653697d2ff5dfdcc17d33cd6db3d3be
Zara <123120853+zaragood@users.noreply.github.com>
2023-11-13 20:49:47 +0000

    i love c

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+// script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer:
+
+const [, , x] = process.argv;
+let i = 0;
+
+if (typeof x === 'string' && !isNaN(x)) {
+  const number = Number(x);
+  while (x > i) {
+    console.log('C is fun');
+    i++;
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
