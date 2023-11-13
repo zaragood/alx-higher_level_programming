ee5aa0de6766826c2c13d7e2e342614abcb38ba0
Zara <123120853+zaragood@users.noreply.github.com>
2023-11-13 20:23:05 +0000

    type conversion

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer:
+
+const [, , myNumber] = process.argv;
+
+if (typeof myNumber === 'string' && !isNaN(myNumber)) {
+  const number = Number(myNumber)
+  console.log('My number: ' + number);
+
+} else {
+  console.log('Not a Number');
+}
