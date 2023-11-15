205b41cf001d11169fc5e94c77421362a44e0fbd
Zara <123120853+zaragood@users.noreply.github.com>
2023-11-15 20:31:47 +0000

    103-object

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,18 @@
+#!/usr/local/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.incr = function () {
+  this.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+
