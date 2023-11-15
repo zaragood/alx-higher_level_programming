eae35ce251cf2c454d5ba67a3eb1bb84d49147d0
Zara <123120853+zaragood@users.noreply.github.com>
2023-11-15 14:34:01 +0000

    anonymous function

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// function that executes x times a function.
+
+const callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+};
+
+module.exports.callMeMoby = callMeMoby;
