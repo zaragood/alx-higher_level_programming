f717df710180b995d851bed60205942cb8133d52
Zara <123120853+zaragood@users.noreply.github.com>
2023-11-15 15:38:24 +0000

    increments and calls function

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// function that increments and calls a function.
+
+let = addMeMaybe = function (number, theFunction) {
+  number++;
+  theFunction(number);
+};
+
+module.exports.addMeMaybe = addMeMaybe;
