92af28a181b5be193fdead40227da8747a9bf2c2
Zara <123120853+zaragood@users.noreply.github.com>
2023-11-17 20:29:15 +0000

    list states in database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,36 +3,17 @@
 import sys
 import MySQLdb
 
-def list_all_states(mysql_username, mysql_password, mysql_database):
-    """make a connection to mysql server to make a connection to the database"""
-    try:
-        mysql_connection = MySQLdb.connect(host='localhost', user=mysql_username, port=3306, db=mysql_database, passwd= mysql_password)
-        
-        """creat a cursor object"""
-        cur = mysql_connection.cursor()
-
-        """execute the SELECT"""
-        cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-        states = cur.fetchall()
-
-        for state in states:
-            print("(%s, '%s')" % state)
-
-        """cleanup"""
-        mysql_connection.close()
-        cur.close()
-    except MySQLdb.Error as e:
-        print(f"Error connecting to MySQL: {e}")      
-
 if __name__ == "__main__":
-    """check if the correct command-line argument is passed"""
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <mysql_username> <mysql_password> <mysql_database>")
-        sys.exit(1)
+    """make a connection to the database hbtn_0e_0_usa"""
+    mysql_connection = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    """extract command-line arguments"""
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    mysql_database = sys.argv[3]
+    """creat a cursor object"""
+    cur = mysql_connection.cursor()
 
-    list_all_states(mysql_username, mysql_password, mysql_database)
+    """execute the SELECT query"""
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    states = cur.fetchall()
+
+    """list all states from the  table"""
+    for state in states:
+        print(state)
