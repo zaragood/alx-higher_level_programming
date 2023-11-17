771c5618c5131eba4db336f018624142c2959789
Zara <123120853+zaragood@users.noreply.github.com>
2023-11-17 21:26:02 +0000

    where name matches the argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""import sys and MySQLdb"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    """make a connection to the database hbtn_0e_0_usa"""
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    """creat a cursor object"""
+    cur = db.cursor()
+
+    """execute the SELECT query"""
+    query = "SELECT * FROM states WHERE name = '{}'".format(sys.argv[4])
+    cur.execute(query)
+    states = cur.fetchall()
+
+    """list all states from the  table"""
+    for state in states:
+        print(state)
+
+    cur.close()
+    db.close()
