c73492bb9c5a7fb04cf29c60c5b94a1079148c9f
Zara <123120853+zaragood@users.noreply.github.com>
2023-11-18 23:05:59 +0000

    All states via SQLAlchemy updated

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+lists all State objects from the database hbtn_0e_6_usa
+"""
 from model_state import Base, State
 import sys
 from sqlalchemy import (create_engine)
@@ -6,17 +9,23 @@ from sqlalchemy.orm import sessionmaker
 
 """script that lists all State objects from the database hbtn_0e_6_usa"""
 if __name__ == "__main__":
+    # establish a connection to the database
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
     )
 
+    # creates a all tables associsted with the Base metadata
     Base.metadata.create_all(engine)
 
+    # create a new session instance bound to the engine
     Session = sessionmaker(bind=engine)
-
     session = Session()
 
+    # Query all State objects from the database and order by states.id
     states_objs = session.query(State).all()
 
+    # Print the State objects in the format specified
     for state in states_objs:
         print(f'{state.id}: {state.name}')
+
+    session.close()
