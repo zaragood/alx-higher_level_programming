35e921957d1101b12d8025eaae7c30fab0f7f92b
Zara <123120853+zaragood@users.noreply.github.com>
2023-11-23 23:27:50 +0000

    updated

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+"""
+script that creates the State “California” with the City “San Francisco” 
+from the database hbtn_0e_100_usa: (100-relationship_states_cities.py)
+"""
 from relationship_city import City, Base
 from relationship_state import State
 import sys
