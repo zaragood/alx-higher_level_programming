27baac5d5085db502a62a21008f66e6d6792f0b6
Zara <123120853+zaragood@users.noreply.github.com>
2023-11-30 23:34:13 +0000

    Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-
-You have to use curl
-You are not allow to use echo, cat, etc. to display the final result
+# Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.You have to use curl You are not allow to use echo, cat, etc. to display the final result
 curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
