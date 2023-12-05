6d2df58c7189f06cf8bf1669ad6f2f21fb432c1b
Zara <123120853+zaragood@users.noreply.github.com>
2023-12-05 23:07:08 +0000

    What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Python script that fetches https://alx-intranet.hbtn.io/status
+You must use the package requests
+You are not allow to import packages other than requests
+The body of the response must be display like the following
+Body response:$
+    - type: <class 'str'>$
+    - content: OK$
+guillaume@ubuntu:~/0x11$
+"""
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print(f"\t- type: {type(r)}")
+    print(f"\t- content: {r}")
