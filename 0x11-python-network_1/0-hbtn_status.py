a950d029df7048660834d3dff1059d97e984368e
Zara <123120853+zaragood@users.noreply.github.com>
2023-12-05 16:24:49 +0000

    What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import urllib.request
+""" Python script that fetches https://alx-intranet.hbtn.io/status
+You must use the package urllib
+You are not allowed to import any packages other than urllib
+The body of the response must be displayed like the following example (tabulation before -)
+You must use a with statement
+"""
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        body = response.read()
+    print("Body response:")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {body.decode('utf8')}")
