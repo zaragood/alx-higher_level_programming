78b79836be159b67cb6e0a6bb61cae6c61682914
zaragood <azaragoodness52@gmail.com>
2024-01-04 11:36:39 +0000

    semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,15 +3,13 @@
 
 const fs = require('fs');
 
-filePath = process.argv[2]
+const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf8', (error, data) => {
-    if (error) {
-        console.log(error);
-        return;
-    }
-    else {
-        // log the content of the file
-        console.log(data);
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    // log the content of the file
+    console.log(data);
+  }
 });
