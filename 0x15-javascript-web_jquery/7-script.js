a72bc2c34076f4dd8124550f0cb04ab084c2c38e
zaragood <azaragoodness52@gmail.com>
2024-01-11 22:34:37 +0000

    updated task 7 and eight

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,5 @@
+// JavaScript script that fetches the character name from this URL: https://swapi-api.alx-tools.com/api/people/5/?format=json
+
+$.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data, status) {
+  $('DIV#character').text(data.name);
+})
